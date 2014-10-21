608d4b915870489043c5b9a58d62838b5e19c146
kristijan <kristijan@koozoo.com>
2014-10-21 15:42:25 +0200

    Remove not needed space.

--- carton/settings.py
+++ carton/settings.py
@@ -4,4 +4,4 @@ CART_SESSION_KEY = getattr(settings, 'CART_SESSION_KEY', 'CART')
 
 CART_TEMPLATE_TAG_NAME = getattr(settings, 'CART_TEMPLATE_TAG_NAME', 'get_cart')
 
-CART_ITEM_CLASS =  getattr(settings, 'CART_ITEM_CLASS', 'carton.cart.CartItem')
+CART_ITEM_CLASS = getattr(settings, 'CART_ITEM_CLASS', 'carton.cart.CartItem')

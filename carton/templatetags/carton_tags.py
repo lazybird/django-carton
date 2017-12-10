96f944bed9470bd04309ac562b86457c9dd69b69
Vinicio Valbuena <formatcomvinicio@gmail.com>
2017-12-09 22:39:17 -0500

    support for django 2.0

--- carton/templatetags/carton_tags.py
+++ carton/templatetags/carton_tags.py
@@ -6,7 +6,6 @@ from carton.settings import CART_TEMPLATE_TAG_NAME
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)
 def get_cart(context, session_key=None, cart_class=Cart):
     """
     Make the cart object available in template.
@@ -21,4 +20,9 @@ def get_cart(context, session_key=None, cart_class=Cart):
     """
     request = context['request']
     return cart_class(request.session, session_key=session_key)
+
+try:
+    register.simple_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
+except ImportError:
+    register.assignment_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
 

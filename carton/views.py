163a344472ee526d70c5019fd8f95cad7a558052
Sylvain <sylvaintoe@gmail.com>
2018-06-25 08:46:20 +0200

    Add doc for cart item destroy

--- carton/views.py
+++ carton/views.py
@@ -82,6 +82,12 @@ class CartViewSet(mixins.ListModelMixin,
         """
         return super().retrieve(request, *args, **kwargs)
 
+    def destroy(self, request, *args, **kwargs):
+        """
+        Removes the cart item given the product ID.
+        """
+        return super().destroy(request, *args, **kwargs)
+
     @action(methods=['post'], detail=True)
     def add(self, request, *args, **kwargs):
         """
